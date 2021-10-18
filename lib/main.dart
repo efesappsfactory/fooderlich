dd2b21ccb0da5e18091bf084068f60ca5be5cfe8
efren sandoval <efren.sandoval@epn.edu.ec>
2021-10-18 12:58:57 -0500

    using the theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
 
 void main() {
   runApp(Fooderlich());
@@ -8,13 +9,25 @@ class Fooderlich extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //final theme = FooderlichTheme.light();
+    final theme = FooderlichTheme.dark();
     return MaterialApp(
-      title: 'Fooderlich',
+      theme: theme,
+      title: "Fooderlich",
       home: Scaffold(
-        appBar: AppBar(title: const Text("Fooderlich")),
-        body: const Center(child: Text("Let's get cooking 👩‍🍳"),),
+        appBar: AppBar(
+          title: Text(
+            "Fooderlich",
+            style: theme.textTheme.headline6,
+          ),
+        ),
+        body: Center(
+          child: Text(
+            "Let's get cooking 👩‍🍳",
+            style: theme.textTheme.headline1,
+          ),
+        ),
       ),
     );
   }
 }
-
