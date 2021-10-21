7ffe0fa238a71afcb34fec69e24ec6ca8041d366
efren sandoval <efren.sandoval@epn.edu.ec>
2021-10-21 10:47:18 -0500

    composing the text

--- lib/card2.dart
+++ lib/card2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'author_card.dart';
+import 'fooderlich_theme.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
@@ -27,6 +28,31 @@ class Card2 extends StatelessWidget {
               authorName: "Efrén Sandoval",
               title: "Smoothie Connoisseur",
               imageProvider: AssetImage("assets/author_katz.jpeg"),
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  Positioned(
+                    bottom: 16,
+                    right: 16,
+                    child: Text(
+                      "Recipe",
+                      style: FooderlichTheme.lightTextTheme.headline1,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 70,
+                    left: 16,
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Text(
+                        "Smoothies",
+                        style: FooderlichTheme.lightTextTheme.headline1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
