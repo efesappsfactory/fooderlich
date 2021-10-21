8f44af951590744588ca11b508579f83b29e4a2d
efren sandoval <efren.sandoval@epn.edu.ec>
2021-10-21 10:01:42 -0500

    adding the IconButton widget

--- lib/author_card.dart
+++ lib/author_card.dart
@@ -19,6 +19,7 @@ class AuthorCard extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(16),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Row(
             children: [
@@ -42,6 +43,15 @@ class AuthorCard extends StatelessWidget {
               ),
             ],
           ),
+          IconButton(
+            icon: const Icon(Icons.favorite_border),
+            iconSize: 30,
+            color: Colors.grey[400],
+            onPressed: () {
+              const snackBar = SnackBar(content: Text("Favorite Pressed"));
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            },
+          )
         ],
       ),
     );
