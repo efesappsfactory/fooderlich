1ec27a7767ea54d95fbfeb06862e9834a58a7599
efren sandoval <efren.sandoval@epn.edu.ec>
2021-10-25 14:35:57 -0500

    new card3

--- lib/components/card3.dart
+++ lib/components/card3.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
+
 import '../fooderlich_theme.dart';
+import '../models/models.dart';
 
 class Card3 extends StatelessWidget {
-  const Card3({Key? key}) : super(key: key);
+  final ExploreRecipe recipe;
+
+  const Card3({
+    Key? key,
+    required this.recipe,
+  }) : super(key: key);
+
+  List<Widget> createTagChips() {
+    final chips = <Widget>[];
+    recipe.tags.take(6).forEach((element) {
+      final chip = Chip(
+        label: Text(
+          element,
+          style: FooderlichTheme.darkTextTheme.bodyText1,
+        ),
+        backgroundColor: Colors.black.withOpacity(0.7),
+      );
+      chips.add(chip);
+    });
+
+    return chips;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +35,14 @@ class Card3 extends StatelessWidget {
           width: 350,
           height: 450,
         ),
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage("assets/mag2.png"),
+            image: AssetImage(recipe.backgroundImage),
             fit: BoxFit.cover,
           ),
-          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+          borderRadius: const BorderRadius.all(
+            Radius.circular(10.0),
+          ),
         ),
         child: Stack(
           children: [
@@ -37,84 +62,18 @@ class Card3 extends StatelessWidget {
                     color: Colors.white,
                     size: 40,
                   ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Text(
-                    "Recipe Trends",
-                    style: FooderlichTheme.darkTextTheme.headline2,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  )
+                  const SizedBox(height: 8),
+                  Text(recipe.title,
+                      style: FooderlichTheme.darkTextTheme.headline2),
+                  const SizedBox(height: 30),
                 ],
               ),
             ),
             Center(
               child: Wrap(
-                alignment: WrapAlignment.start,
-                spacing: 12,
-                children: [
-                  Chip(
-                    label: Text(
-                      "Healthy",
-                      style: FooderlichTheme.darkTextTheme.bodyText1,
-                    ),
-                    backgroundColor: Colors.black.withOpacity(0.7),
-                    onDeleted: () => print("delete"),
-                  ),
-                  Chip(
-                    label: Text(
-                      "Vegan",
-                      style: FooderlichTheme.darkTextTheme.bodyText1,
-                    ),
-                    backgroundColor: Colors.black.withOpacity(0.7),
-                    onDeleted: () => print("delete"),
-                  ),
-                  Chip(
-                    label: Text(
-                      "Carrots",
-                      style: FooderlichTheme.darkTextTheme.bodyText1,
-                    ),
-                    backgroundColor: Colors.black.withOpacity(0.7),
-                  ),
-                  Chip(
-                    label: Text(
-                      "Chicken",
-                      style: FooderlichTheme.darkTextTheme.bodyText1,
-                    ),
-                    backgroundColor: Colors.black.withOpacity(0.7),
-                  ),
-                  Chip(
-                    label: Text(
-                      "Milk",
-                      style: FooderlichTheme.darkTextTheme.bodyText1,
-                    ),
-                    backgroundColor: Colors.black.withOpacity(0.7),
-                  ),
-                  Chip(
-                    label: Text(
-                      "Cake",
-                      style: FooderlichTheme.darkTextTheme.bodyText1,
-                    ),
-                    backgroundColor: Colors.black.withOpacity(0.7),
-                  ),
-                  Chip(
-                    label: Text(
-                      "Salad",
-                      style: FooderlichTheme.darkTextTheme.bodyText1,
-                    ),
-                    backgroundColor: Colors.black.withOpacity(0.7),
-                  ),
-                  Chip(
-                    label: Text(
-                      "Hamburger",
-                      style: FooderlichTheme.darkTextTheme.bodyText1,
-                    ),
-                    backgroundColor: Colors.black.withOpacity(0.7),
-                  ),
-                ],
-              ),
+                  alignment: WrapAlignment.start,
+                  spacing: 12,
+                  children: createTagChips()),
             ),
           ],
         ),
