1fe279147a10d264190ff7146e8a8e2f8879100f
efren sandoval <efren.sandoval@epn.edu.ec>
2021-10-25 21:23:13 -0500

    new tab icons

--- lib/home.dart
+++ lib/home.dart
@@ -12,10 +12,17 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int _selectedIndex = 0;
+
   static List<Widget> pages = <Widget>[
-    const Card1(),
-    const Card2(),
-    const Card3(),
+    Container(
+      color: Colors.white,
+    ),
+    Container(
+      color: Colors.green,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
   ];
 
   void _onItemTapped(int index) {
@@ -38,16 +45,16 @@ class _HomeState extends State<Home> {
         onTap: _onItemTapped,
         items: <BottomNavigationBarItem>[
           const BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: "Card",
+            icon: Icon(Icons.explore),
+            label: "Explore",
           ),
           const BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: "Card2",
+            icon: Icon(Icons.book),
+            label: "Recipes",
           ),
           const BottomNavigationBarItem(
-            icon: Icon(Icons.card_giftcard),
-            label: "Card3",
+            icon: Icon(Icons.list),
+            label: "To Buy",
           )
         ],
       ),
