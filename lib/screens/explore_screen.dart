b419f29b398f06399555a94475a44869ba72c6a0
efren sandoval <efren.sandoval@epn.edu.ec>
2021-10-26 21:02:58 -0500

    explore screen init

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../components/components.dart';
+import '../models/models.dart';
+import '../api/mock_fooderlich_service.dart';
+
+class ExploreScreen extends StatelessWidget {
+  final mockService = MokcFooderlichService();
+  ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Explore Screen"),
+    );
+  }
+}
