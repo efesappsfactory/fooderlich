f9427bcc7208772b59c3ddf33fc89ec39526e0a5
efren sandoval <efren.sandoval@epn.edu.ec>
2021-10-29 08:15:07 -0500

    headline1 to headline2

--- lib/components/friend_post_list_view.dart
+++ lib/components/friend_post_list_view.dart
@@ -21,7 +21,7 @@ class FriendPostListView extends StatelessWidget {
         children: [
           Text(
             "Social Chefs 👩‍🍳",
-            style: Theme.of(context).textTheme.headline1,
+            style: Theme.of(context).textTheme.headline2,
           ),
           const SizedBox(
             height: 16,
