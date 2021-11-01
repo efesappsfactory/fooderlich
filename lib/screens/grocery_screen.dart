4f5daa6ed8a0bd28feb55f9a832315f8b0c64d19
efren sandoval <efren.sandoval@epn.edu.ec>
2021-11-01 12:54:47 -0500

    consuming the changes

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'empty_grocery_screen.dart';
+import '../models/models.dart';
 
 class GroceryScreen extends StatelessWidget {
-  const GroceryScreen({ Key? key }) : super(key: key);
+  const GroceryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return EmptyGroceryScreen();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
+      ),
+      body: buildGroceryScreen(),
+    );
+  }
+
+  Widget buildGroceryScreen() {
+    return Consumer<GroceryManager>(
+      builder: (context, manager, child) {
+        if (manager.groceryItems.isNotEmpty) {
+          return Container();
+        } else {
+          return const EmptyGroceryScreen();
+        }
+      },
+    );
   }
 }
