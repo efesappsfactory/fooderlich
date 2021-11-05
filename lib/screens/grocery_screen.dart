8af771678353883f569945c4b4e89d6a1a415be3
efren sandoval <efren.sandoval@epn.edu.ec>
2021-11-05 15:51:09 -0500

    GroceryListScreen init

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'empty_grocery_screen.dart';
 import '../models/models.dart';
 import 'grocery_item_screen.dart';
+import 'grocery_list_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
@@ -39,7 +40,7 @@ class GroceryScreen extends StatelessWidget {
     return Consumer<GroceryManager>(
       builder: (context, manager, child) {
         if (manager.groceryItems.isNotEmpty) {
-          return Container();
+          return GroceryListScreen(manager: manager);
         } else {
           return const EmptyGroceryScreen();
         }
