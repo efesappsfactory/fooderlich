fde7bf8cf7923f6c7faa560d61d4555a01a18420
efren sandoval <efren.sandoval@epn.edu.ec>
2021-11-09 13:01:14 -0500

    main from stateless to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'fooderlich_theme.dart';
 import 'home.dart';
-import 'package:provider/provider.dart';
 import 'models/models.dart';
 
 void main() {
   runApp(Fooderlich());
 }
 
-class Fooderlich extends StatelessWidget {
+class Fooderlich extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _FooderlichState createState() => _FooderlichState();
+}
+
+class _FooderlichState extends State<Fooderlich> {
   @override
   Widget build(BuildContext context) {
     //final theme = FooderlichTheme.light();
