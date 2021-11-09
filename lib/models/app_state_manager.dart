a894f6ba0661c1ec90c459e19fdce66468429ccb
efren sandoval <efren.sandoval@epn.edu.ec>
2021-11-09 14:39:11 -0500

    managing app state init

---
+++ lib/models/app_state_manager.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class FooderlichTab {
+  static const int explore = 0;
+  static const int recipes = 1;
+  static const int toBuy = 2;
+}
+
+class AppStateManager extends ChangeNotifier {
+  bool _initialized = false;
+  bool _loggedIn = false;
+  bool _onboardingComplete = false;
+  int _selectedTab = FooderlichTab.explore;
+
+  bool get isInitialized => _initialized;
+  bool get isLoggedIn => _loggedIn;
+  bool get isOnboardingComplete => _onboardingComplete;
+  int get getSelectedTab => _selectedTab;
+}
