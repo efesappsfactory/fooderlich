4504dd69cfbe518bd8958a87fb77bf4d16b1b534
efren sandoval <efren.sandoval@epn.edu.ec>
2021-11-15 15:59:37 -0500

    home screen updated

---
+++ lib/screens/home.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'explore_screen.dart';
+import 'recipes_screen.dart';
+import 'grocery_screen.dart';
+
+class Home extends StatefulWidget {
+  final int currentTab;
+  const Home({
+    Key? key,
+    required this.currentTab,
+  }) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  static List<Widget> pages = <Widget>[
+    ExploreScreen(),
+    RecipesScreen(),
+    const GroceryScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Fooderlich",
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        actions: [
+          profileButton(),
+        ],
+      ),
+      body: IndexedStack(
+        index: widget.currentTab,
+        children: pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
+        currentIndex: widget.currentTab,
+        onTap: (index) {},
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: "Explore",
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: "Recipes",
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: "To Buy",
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget profileButton() {
+    return Padding(
+      padding: const EdgeInsets.only(right: 16.0),
+      child: GestureDetector(
+        child: const CircleAvatar(
+          backgroundColor: Colors.transparent,
+          backgroundImage: AssetImage("assets/profile_pics/person_stef.jpeg"),
+        ),
+        onTap: () {},
+      ),
+    );
+  }
+}
