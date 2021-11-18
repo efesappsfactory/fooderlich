f3f0bf78138c4638baa8f4383e3c4574ad917978
efren sandoval <efren.sandoval@epn.edu.ec>
2021-11-18 14:18:58 -0500

    exporting profile and onboarding screens

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -3,3 +3,5 @@ export 'login_screen.dart';
 export 'home.dart';
 export 'grocery_list_screen.dart';
 export 'grocery_item_screen.dart';
+export 'onboarding_screen.dart';
+export 'profile_screen.dart';
